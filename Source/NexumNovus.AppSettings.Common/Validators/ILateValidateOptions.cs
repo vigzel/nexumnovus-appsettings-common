2667b94eedef68131f37300c92ac4884c061c480
vigzel <vigzel@users.noreply.github.com>
2023-05-25 11:53:11 +0200

    returned late validator, default IValidateOptions always get called when options are accessed

---
+++ Source/NexumNovus.AppSettings.Common/Validators/ILateValidateOptions.cs
@@ -0,0 +1,11 @@
+namespace NexumNovus.AppSettings.Common.Validators;
+
+using Microsoft.Extensions.Options;
+
+/// <summary>
+/// Interface used to validate options.
+/// </summary>
+/// <typeparam name="TOptions">The options type to validate.</typeparam>
+public interface ILateValidateOptions<TOptions> : IValidateOptions<TOptions> where TOptions : class
+{
+}
